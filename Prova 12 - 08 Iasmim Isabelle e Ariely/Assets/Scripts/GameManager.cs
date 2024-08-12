cc606313783b0fe2f299850f0f05498202e5d02f
pssgiasmim <iasmimpassing@gmail.com>
2024-08-12 09:14:12 -0300

    1. Criação da classe GameManager declaração de variáveis

---
+++ Prova 12 - 08 Iasmim Isabelle e Ariely/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    #region Singleton
+    public static GameManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    int corDaVez;
+    int acertos;
+    int erros;
+
+    int[] sequencia;
+
+    [SerializeField] string[] nomes;
+
+    void Start()
+    {
+        
+    }
+
+   
+}
