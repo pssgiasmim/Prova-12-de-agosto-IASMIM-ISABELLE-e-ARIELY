5ca34b18c1b354e48bd68ff3a9e9289464b4eadd
pssgiasmim <iasmimpassing@gmail.com>
2024-08-12 10:06:32 -0300

    Método GerarSequencia

--- Prova 12 - 08 Iasmim Isabelle e Ariely/Assets/Scripts/GameManager.cs
+++ Prova 12 - 08 Iasmim Isabelle e Ariely/Assets/Scripts/GameManager.cs
@@ -26,6 +26,27 @@ public class GameManager : MonoBehaviour
         GerarSequencia();
     }
 
+    void GerarSequencia()
+    {
+        corDaVez = 0;
+
+        int[] sequencia = new int [Random.Range(3,nomes.Length)];
+
+        UIManager.instance.LimparTexto();
+
+        for (int i = 0; i < sequencia.Length; i++)
+        {
+            sequencia[i] = Random.Range(0, nomes.Length);
+        }
+
+    }
+
+    public void ChecarCor( int corIndex)
+    {
+
+    }
+
+
 
    
 }
